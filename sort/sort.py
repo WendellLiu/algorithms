3cd7eeb0b9db259222e56589faa2be2eb175e945
wendellliu <cuk.bas@gmail.com>
2016-04-21 14:25:49 +0800

    merge_sort

--- sort/sort.py
+++ sort/sort.py
@@ -25,3 +25,41 @@ def insertion_sort(array, asc=True):
                 pivot -= 1
 
     return array
+
+def merge_sort(array, asc=True):
+    begin = 0
+    end = len(array)-1
+
+    if len(array) > 1:
+        # partition
+        l = array[begin:len(array)/2]
+        r = array[len(array)/2:end+1]
+        l = merge_sort(l, asc)
+        r = merge_sort(r, asc)
+
+    else:
+        return array
+
+    return merge(l, r, asc)
+
+def merge(sorted_l, sorted_r, asc):
+
+    l_index = 0
+    r_index = 0
+    result = [0] * (len(sorted_l+sorted_r))
+
+    i = 0
+    while True:
+        if (l_index < len(sorted_l)) and (r_index < len(sorted_r)):
+            if (sorted_l[l_index] <= sorted_r[r_index]) == asc:
+                result[i] = sorted_l[l_index]
+                l_index += 1
+                i += 1
+            else:
+                result[i] = sorted_r[r_index]
+                r_index +=1
+                i += 1
+        else:
+            result[i:] = sorted_l[l_index:] if r_index == len(sorted_r) else sorted_r[r_index:]
+            return result
+
